c9a024e25d05b0c3d971bd38382146664527222f
rasikachavan751-sudo <rasikachavan751@gmail.com>
2025-09-15 19:01:20 +0530

    Add files via upload

---
+++ rasika13.c
@@ -0,0 +1,11 @@
+
+
+#include <stdio.h>
+int main() {
+    char ch='e';
+    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+        printf("Vowel");
+    else
+        printf("Consonant");
+    return 0;
+}
